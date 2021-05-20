b96b38db750c113a3dacaa8912c40df5008ed398
Visarg <visarg3009@gmail.com>
2021-05-20 20:20:16 +0530

    Added payment method

---
+++ magento/app/code/local/Test/Demo2/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class Test_Demo2_Helper_Data extends Mage_Core_Helper_Data
+{
+}
---
+++ magento/app/code/local/Test/Demo2/Model/Cod.php
@@ -0,0 +1,5 @@
+<?php
+class Test_Demo2_Model_Cod extends Mage_Payment_Model_Method_Abstract
+{
+    protected $_code = 'cod';
+}
